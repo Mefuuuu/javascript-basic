d032ecc81154eabfa6a5646f1d85a3305d61776f
Mefuuuu <133778142+Mefuuuu@users.noreply.github.com>
2024-04-27 10:35:19 +0700

    Add files via upload

--- video11 (c3 v1)/video11.js
+++ video11 (c3 v1)/video11.js
@@ -1,3 +1,13 @@
 // #15 For Loops - Làm Quen Với Vòng Lặp For
 
 console.log('hello world')
+
+for(let i = 1; i <= 10; i++){
+    console.log(`check variable i =`, i);
+}
+
+let arr = ['Mu', 'Mancity', 'Liver', 'Chelsea']
+for(let i = 0; i < arr.length; i++){
+    console.log(`check arr = `,arr[i]);
+}
+
