460649397c5c658686b5d91342ec29b2e4e607ff
Mefuuuu <133778142+Mefuuuu@users.noreply.github.com>
2024-05-01 01:06:19 +0700

    Add files via upload

---
+++ video23 (c5 v3)/video23.js
@@ -0,0 +1,13 @@
+console.log(`#27 Reduce - Giải Pháp Hiệu Năng Trong Thế Giới Xi Đa`)
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
